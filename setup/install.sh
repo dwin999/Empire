5adbd682f3d527d4f65149baec5e8ebbc3f3b930
dwin999 <winfreyd@uni.coventry.ac.uk>
2015-09-06 18:21:31 +0100

    Add check for root,stopped calling setup_python
    Added a check to see if the user is running as root before they install.
    
    Calling the setup_python script this way causes errors when following the quick start.

--- setup/install.sh
+++ setup/install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
 apt-get install python-pip
 
 #ubuntu 14.04 LTS dependencies
@@ -11,5 +15,3 @@ pip install pycrypto
 #kali dependencies
 pip install iptools
 pip install pydispatcher
-
-./setup_database.py
